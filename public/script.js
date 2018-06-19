72e4008f828322ac4199be81665e73e3cbd8c854
Laura Schneider <laurie.schneider@gmail.com>
2018-06-19 19:36:28 +0200

    form addition

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
 });
+
+
+//FORM SECTION
+
+
+//
+// input.on('click', function(e) {
+//     e.preventDefault();
+//     const input = $('input[name="name"]'.val());
+//
+//     console.log(input);
+// }
