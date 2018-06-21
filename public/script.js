3a10894f5eac88a8203d9721303835288da970c0
lorettalevin <lorettalevin4@gmail.com>
2018-06-21 22:56:58 +0200

    added new js file and fixed contact button

--- public/script.js
+++ public/script.js
@@ -1,65 +1,11 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
-    $('#contact-button').click(function() {
-        $('html, body').animate({
-            scrollTop: $(document).height()
-        }, 'slow');
-        return false;
-    });
-
-});
-
-var projects = document.getElementsByClassName("project");
-var dots = document.getElementsByClassName("dot");
-var isTransitioning = false;
-var curProject = 0;
-var onDeck = 1;
-var timerID;
-
-function moveProjects() {
-    projects[curProject].classList.remove("onscreen");
-    projects[curProject].classList.add("exit");
-    projects[onDeck].classList.add("onscreen");
-
-    setDot();
-
-    curProject = onDeck;
-    onDeck += 1;
-
-    if (onDeck >= projects.length) {
-        onDeck = 0;
+    if (location.pathname !== '/loretta' && location.pathname !== '/laura') {
+        $('#contact-button').click(function() {
+            $('html, body').animate({
+                scrollTop: $(document).height()
+            }, 'slow');
+            return false;
+        });
     }
-    isTransitioning = true;
-}
-
-function setDot() {
-    for (var i = 0; i < dots.length; i++) {
-        dots[i].classList.remove("current-dot");
-    }
-    dots[onDeck].classList.add("current-dot");
-}
-
-timerID = setTimeout(moveProjects, 3000);
-
-document.addEventListener("transitionend", function(e) {
-    if (!e.target.classList.contains("exit")) {
-        return;
-    }
-    e.target.classList.remove("exit");
-    timerID = setTimeout(moveProjects, 3000);
-    isTransitioning = false;
-});
-
-dots = [].slice.call(dots);
-dots.forEach(function(dot, i) {
-    dot.addEventListener("click", function() {
-        if (isTransitioning) {
-            return;
-        }
-        clearTimeout(timerID);
-
-        onDeck = i;
-
-        moveProjects();
-    });
 });
